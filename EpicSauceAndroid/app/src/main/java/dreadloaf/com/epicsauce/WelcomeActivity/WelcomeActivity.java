cc2639db502bf40e77447a68c6d13d9ee202d37c
daviddsouza2000 <1david.dsouza.2000@gmail.com>
2019-02-09 19:00:11 -0500

    Welcome page part 2

--- EpicSauceAndroid/app/src/main/java/dreadloaf/com/epicsauce/WelcomeActivity/WelcomeActivity.java
+++ EpicSauceAndroid/app/src/main/java/dreadloaf/com/epicsauce/WelcomeActivity/WelcomeActivity.java
@@ -1,15 +1,40 @@
 package dreadloaf.com.epicsauce.WelcomeActivity;
 
+import android.content.Intent;
+import android.graphics.ImageDecoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
+import de.hdodenhof.circleimageview.CircleImageView;
+import dreadloaf.com.epicsauce.MainActivity;
 import dreadloaf.com.epicsauce.R;
+import dreadloaf.com.epicsauce.SauceOfDay.SauceOfDayActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
+    ImageButton sodButton;
+    ImageButton foodSelectionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        sodButton = findViewById(R.id.sod_button);
+        foodSelectionButton = findViewById(R.id.food_selection_button);
+
+        sodButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(WelcomeActivity.this, SauceOfDayActivity.class));
+            }
+        });
+
+        foodSelectionButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            }
+        });
     }
 }
