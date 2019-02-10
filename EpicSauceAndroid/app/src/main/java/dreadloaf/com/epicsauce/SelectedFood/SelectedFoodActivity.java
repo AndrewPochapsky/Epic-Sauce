dbd34600a2c050ae3a7a895e33f44d70698ce330
SSJANSON <jason.lam.qwerty@gmail.com>
2019-02-10 00:00:08 -0500

    sliding thing

--- EpicSauceAndroid/app/src/main/java/dreadloaf/com/epicsauce/SelectedFood/SelectedFoodActivity.java
+++ EpicSauceAndroid/app/src/main/java/dreadloaf/com/epicsauce/SelectedFood/SelectedFoodActivity.java
@@ -2,9 +2,9 @@ package dreadloaf.com.epicsauce.SelectedFood;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 
 import dreadloaf.com.epicsauce.R;
-
 
 public class SelectedFoodActivity extends AppCompatActivity {
     @Override
--- EpicSauceAndroid/app/src/main/java/dreadloaf/com/epicsauce/WelcomeActivity/WelcomeActivity.java
+++ EpicSauceAndroid/app/src/main/java/dreadloaf/com/epicsauce/WelcomeActivity/WelcomeActivity.java
@@ -1,15 +1,24 @@
 package dreadloaf.com.epicsauce.WelcomeActivity;
 
+import android.gesture.Gesture;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+
+import android.widget.TextView;
 
 
 import dreadloaf.com.epicsauce.R;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
     }
+
 }
