dc87e07e33c18e137e641e300ca8b1b764c27113
SSJANSON <jason.lam.qwerty@gmail.com>
2019-02-10 05:41:07 -0500

    LOL

--- EpicSauceAndroid/app/src/main/java/dreadloaf/com/epicsauce/WelcomeActivity/WelcomeActivity.java
+++ EpicSauceAndroid/app/src/main/java/dreadloaf/com/epicsauce/WelcomeActivity/WelcomeActivity.java
@@ -3,22 +3,44 @@ package dreadloaf.com.epicsauce.WelcomeActivity;
 import android.gesture.Gesture;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.GestureDetector;
-
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import android.view.View;
 import android.widget.TextView;
 
+
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import dreadloaf.com.epicsauce.R;
 
 public class WelcomeActivity extends AppCompatActivity {
-
-
-
+//    private SlidingUpPanelLayout mLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+//        mLayout = findViewById(R.id.sliding_layout);
+//        if(mLayout.getPanelState() == SlidingUpPanelLayout.PanelState.HIDDEN){
+//            Log.d("GAYBITCH", "HIIII");
+//        }
 
+//        SlidingUpPanelLayout l = new SlidingUpPanelLayout(this);
+//
+//        l.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+//            @Override
+//            public void onPanelSlide(View panel, float slideOffset) {
+//
+//            }
+//
+//            @Override
+//            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+//                Log.e("Listener: ", "hi");
+//            }
+//        });
     }
 
+
+
 }
+
